bd5e06e188818230ce5cf56b579e4ba70d999b2b
buck8bunny <monstera211@gmail.com>
2023-12-08 12:52:39 +0200

    Fixed display of categories on mobile devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => {
+    setIsNavCollapsed(!isNavCollapsed);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light rounded">
       <div className="container">
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link to="/" className="nav-link btn btn-light">Головна</Link>
+              <Link to="/" className="nav-link btn btn-light" onClick={handleNavCollapse}>
+                Головна
+              </Link>
             </li>
             <li className="nav-item">
-              <Link to="/calendar" className="nav-link btn btn-light">Календар</Link>
+              <Link to="/calendar" className="nav-link btn btn-light" onClick={handleNavCollapse}>
+                Календар
+              </Link>
             </li>
             <li className="nav-item">
-              <Link to="/stats" className="nav-link btn btn-light">Статистика</Link>
+              <Link to="/stats" className="nav-link btn btn-light" onClick={handleNavCollapse}>
+                Статистика
+              </Link>
             </li>
           </ul>
         </div>
